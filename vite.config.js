ee6ed44275135d78a21aa4db81778f3fb3047158
vineethpradeep <vineethpadeeps@gmail.com>
2025-06-06 10:41:06 +0100

    vite_config_update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: "public/frontend/assets",
+        rollupOptions: {
+            input: {
+                app: "resources/js/app.js",
+            },
+            output: {
+                entryFileNames: "js/[name].js",
+                chunkFileNames: "js/[name].js",
+                assetFileNames: "css/[name].[ext]",
+            },
+        },
+    },
 });
