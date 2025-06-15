cf678c4d2c3a4b0da1690fd90a1cff0b513770c7
vineethpradeep <vineethpadeeps@gmail.com>
2025-06-15 18:37:34 +0100

    remove defer

--- resources/views/frontend/frontend-landingpage.blade.php
+++ resources/views/frontend/frontend-landingpage.blade.php
@@ -166,7 +166,7 @@
     <script src="{{asset('frontend/assets/lib/waypoints/waypoints.min.js')}}"></script>
     <script src="{{asset('frontend/assets/lib/owlcarousel/owl.carousel.min.js')}}"></script>
     <!-- Template Javascript -->
-    <script src="{{asset('frontend/assets/js/main.js')}}" defer></script>
+    <script src="{{asset('frontend/assets/js/main.js')}}"></script>
     <script src="{{asset('frontend/assets/js/plugins.js')}}"></script>
     <!-- Bootstrap 5 Bundle (includes Popper) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
